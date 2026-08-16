247adb38e34ebfc10eda14f06b44d36365624646
bl4z3 <bl4z3@bl4z3-latitude5400>
2026-08-16 12:25:10 +0530

    exports dao and hiberate sub-packges

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.blaze_technologies.retail_manager {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires jakarta.persistence;
+	requires org.hibernate.orm.core;
 
 	opens org.blaze_technologies.retail_manager.data_types
 			to org.blaze_technologies.retail_manager.controllers;
@@ -10,5 +11,6 @@ module org.blaze_technologies.retail_manager {
 	exports org.blaze_technologies.retail_manager;
 	exports org.blaze_technologies.retail_manager.controllers;
 	exports org.blaze_technologies.retail_manager.data_types;
-	exports org.blaze_technologies.retail_manager.models;
+	exports org.blaze_technologies.retail_manager.dao;
+	exports org.blaze_technologies.retail_manager.hibernate;
 }
